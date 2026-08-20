8b905a98cdb1be65728d191a1d767453508e40b9
boubou19 <miisterunknown@gmail.com>
2026-08-20 07:39:48 +0200

    reduce gene matcher allocation

--- src/main/scala/net/bdew/gendustry/forestry/GeneRecipe.scala
+++ src/main/scala/net/bdew/gendustry/forestry/GeneRecipe.scala
@@ -20,20 +20,33 @@ class GeneRecipe extends IRecipe {
     getCraftingResult(inv) != null
   def getCraftingResult(inv: InventoryCrafting): ItemStack = {
     var template: ItemStack = null
-    var samples = Seq.empty[GeneSampleInfo]
-    for (i <- 0 until 3; j <- 0 until 3) {
-      val itm = inv.getStackInRowAndColumn(i, j)
-      if (itm != null && itm.getItem == GeneSample && itm.hasTagCompound)
-        samples :+= GeneSample.getInfo(itm)
-      else if (itm != null && itm.getItem == GeneTemplate && template == null)
-        template = itm
-      else if (itm != null)
-        return null
+    var samples: Array[GeneSampleInfo] = null
+    var sampleCount = 0
+    var i = 0
+    while (i < 3) {
+      var j = 0
+      while (j < 3) {
+        val itm = inv.getStackInRowAndColumn(i, j)
+        if (itm != null && itm.getItem == GeneSample && itm.hasTagCompound) {
+          val sample = GeneSample.getInfo(itm)
+          if (sample.root == null || sample.allele == null) return null
+          if (samples == null) samples = new Array[GeneSampleInfo](9)
+          samples(sampleCount) = sample
+          sampleCount += 1
+        } else if (
+          itm != null && itm.getItem == GeneTemplate && template == null
+        ) template = itm
+        else if (itm != null) return null
+        j += 1
+      }
+      i += 1
     }
-    if (samples.isEmpty || template == null) return null
+    if (sampleCount == 0 || template == null) return null
     val out = template.copy()
-    for (s <- samples) {
-      if (!GeneTemplate.addSample(out, s)) return null
+    i = 0
+    while (i < sampleCount) {
+      if (!GeneTemplate.addSample(out, samples(i))) return null
+      i += 1
     }
     return out
   }
